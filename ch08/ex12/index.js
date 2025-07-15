de48d80d2dbff7caac09649f68602b302ef46a54
kawPenguin <kawpenguin@gmail.com>
2025-07-16 07:59:04 +0900

    細かい修正

--- ch08/ex12/index.js
+++ ch08/ex12/index.js
@@ -1,7 +1,6 @@
 export function f(bodyStr) {
   const args = [];
-  
-  // より正確な最大引数番号の検索
+
   let maxArgNum = 0;
   const matches = bodyStr.match(/\$(\d+)/g);
   if (matches) {
@@ -18,7 +17,6 @@ export function f(bodyStr) {
     args.push(`arg${i}`);
   }
   
-  // プレースホルダーの置換
   let processedBody = bodyStr;
   for (let i = 1; i <= maxArgNum; i++) {
     processedBody = processedBody.replace(new RegExp(`\\$${i}`, 'g'), `arg${i}`);
